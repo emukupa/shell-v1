0d9d5f9b5ef1dccbdc1b88bde9a0ddbee521f890
emukupa1 <edward-manda@uhatok.com>
2022-05-23 03:02:38 -0500

    added the read shell

--- run.c
+++ run.c
@@ -1,21 +1,54 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "run.h"
+
+char *read_shell(void)
+{
+    char *line = NULL;
+    size_t buffer_size = 0; // getline will allocate a buffer for us
+    if (getline(&line, &buffer_size, stdin) == -1)
+    {
+        if (feof(stdin))
+        {
+            exit(EXIT_SUCCESS);
+        }
+        else
+        {
+            perror("shell: readline\n");
+            exit(EXIT_FAILURE);
+        }
+    }
+    puts("read");
+    return line;
+}
 
 void run()
 {
-    puts("execute commands!");
-    /**
-     * @brief Read:
-     * Read the command from standard input.
-     */
+    char *line;
+    // char **args;
+    int status;
 
-    /**
-     * @brief Parse:
-     * Separate the command string into a program and arguments.
-     */
+    // loop until status exits
+    do
+    {
+        printf("> ");
+        /**
+         * @brief Read:
+         * Read the command from standard input.
+         */
+        line = read_shell();
 
-    /**
-     * @brief Execute:
-     * Run the parsed command.
-     */
+        /**
+         * @brief Parse:
+         * Separate the command string into a program and arguments.
+         */
+        // args = split_line(line);
+        // printf("First argument is %s", args[0]);
+
+        /**
+         * @brief Execute:
+         * Run the parsed command.
+         */
+        status = 0;
+    } while (status);
 }
--- run.h
+++ run.h
@@ -1,5 +1,6 @@
 #ifndef RUN
 #define RUN
+char *read_shell(void);
 void run(void);
-int n;
+
 #endif // !RUN
